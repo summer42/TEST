759b703b232245d0484a2cb57b3a8f5240c12601
xuning <ning.xu@85ido.com>
2017-04-22 15:08:01 +0800

    isloading 支持链式调用和回调

--- AngularJs-Stu/IsLoading/queryService.js
+++ AngularJs-Stu/IsLoading/queryService.js
@@ -24,52 +24,83 @@
                 }
             ]
             var PostQuery = function (url) {
-                return function (fn, param) {
-                    var defer = $q.defer();
+                return function (fn, param, errFn) {
+                    var callback = fn;
+                    var _param = param;
                     var _this = this;
+                    var defer = $q.defer();
                     _this.isLoading = false;
                     _this.query = function () {
                         _this.isLoading = true;
                         // $http.post(url, param)
+                        //reverse params
+                        // if(typeof fn !='function'&&typeof param =='function') {
+                        //     var callback = param;
+                        //     var _param = fn;
+                        // }
                         $q((resolve, reject) => {
-                            setTimeout(function () { resolve([]) }, 1500)
+                            setTimeout(function () { resolve(data) }, 1500)
                         }).then(function (result) {
                             _this.isLoading = false;
-                            fn(result);
+                            if (!callback || typeof callback != 'function') {
+                                defer.resolve(result);
+                            }
+                            else if (fn && typeof fn == 'function') {
+                                fn(result);
+                            }
+                        }).catch(err => {
+                            if (typeof errFn == 'function') {
+                                errFn(err)
+                            }
+                            else {
+                                console.log(err);
+                            }
                         })
+                        return defer.promise;
                     }
                 }
             }
             this.PersenalSummary = PostQuery('/management/statistics/offline-line/logs');
+            this.test2 = PostQuery('');
         })
         .controller('IsLoadingController', function ($timeout, $scope, queryService) {
+            this.$onInit = function () {
+                $scope.users.query();
+            }
+
             var test = queryService.PersenalSummary;
             $scope.users = new test(function (data) {
                 $scope.users.list = data;
             });
-            this.$onInit = function () {
-                $scope.users.query();
-            }
+            $scope.users = new test();
+            // $scope.users.query().then(function (data) {
+            //     $scope.users.list = data;
+            // })
+
+
+
         })
         .directive('tableList', function () {
             return {
                 restrict: 'A',
                 transclude: true,
                 link: function (scope, attrs, ele) {
-                    var tdCount = ele.$$element[0].querySelector('thead tr').childElementCount;
+                    // var tdCount = ele.$$element[0].querySelector('thead tr').childElementCount;
                 },
                 scope: {
                     tableObj: '='
                 },
                 template: `                    
-                    <ng-transclude>
+                   
+                    <ng-transclude></ng-transclude ng-cloak>
                     <tr ng-if='tableObj.isLoading'>
-                        <td colspan='99' style='text-align:center;'>正在加载</td>
+                        <td colspan='3' style='text-align:center;'>正在加载</td>
                     </tr>
                      <tr ng-if='!tableObj.isLoading&&tableObj.list.length == 0' ng-cloak>
-                        <td colspan='99' style='text-align:center;'>暂无数据</td>
+                        <td colspan='3' style='text-align:center;'>暂无数据</td>
                     </tr>
-                    </ng-transclude ng-cloak>
+                     
+                    
                 `
             }
         })
--- Babel/gulpfile.babel.js
+++ Babel/gulpfile.babel.js
@@ -1,93 +1,52 @@
-import babelHelpers from './helper';
-console.log(babelHelpers);
 import gulp from 'gulp';
-// import jshint from 'gulp-jshint';
 import babel from 'gulp-babel';
 import uglify from 'gulp-uglify';
-import stripDebug from 'gulp-strip-debug';
-import cssnano from 'gulp-cssnano';
+import concat from 'gulp-concat';
 import rev from 'gulp-rev';
 import revCollector from 'gulp-rev-collector';
-import ngAnnotate from 'gulp-ng-annotate';
-import templatecache from 'gulp-angular-templatecache';
+import sourcemaps from 'gulp-sourcemaps';
 import useref from 'gulp-useref';
 import gulpif from 'gulp-if';
-import revReplace from 'gulp-rev-replace';
-import cached from 'gulp-cached';
-import preprocess from 'gulp-preprocess';
-// import del from 'del';
-import runSequence from 'run-sequence';
-import { projectConfig } from './package.json';
-
-// import 'babel-core/register'
-// import 'babel/dist/external-helpers'
-// var babelHelpers = require('helper');
-const client = {
-    "js": {
-        "src": "source/**/*.js",
-        "dist": "js/"
-    }
-};
+import cssmin from 'gulp-minify-css';
 
 
-/**
- * 将 options.src 的内容复制到 options.dist，没有内容则不生成目录。
- *
- * @param {Object} options 配置对象。
- * @param {string|array.<string>} options.src 源内容
- * @param {string} options.dist 输出目录
- */
-const copy = ({ src, dist }) =>
-    gulp.src(src)
-        .pipe(gulp.dest(dist));
-
-/**
- * 验证并转换前端 js。
- */
-gulp.task('transpile', [],
-    () => gulp.src(client.js.src)
-        .pipe(cached('transpile'))
-        .pipe(babel({
-            "presets": [
-                "es2015"
-            ],
-            "plugins": [
-                "external-helpers"
-            ]
-        }))
-        .pipe(preprocess({
-            context: {
-                NODE_ENV: 'development'
-            }
-        }))
-        .on('error', function (e) {
-            console.log([
-                '-----------transpile error-----------\n',
-                e.name,
-                ': ',
-                e.message,
-                '\n',
-                e.codeFrame,
-                '\n',
-                '-----------transpile error-----------'
-            ].join(''));
-            this.emit('end');
-        })
-        .pipe(ngAnnotate({
-            single_quotes: true
-        }))
-        .pipe(gulp.dest(client.js.dist))
-);
-
-/**
- * watch transpile。
- */
 gulp.task('watch', ['transpile'], () => {
-    /*var watcher = */gulp.watch(client.js.src, ['transpile']);
-    // gulp.watch(client.template.src, ['partial-html']);
-    // watcher.on('change', function(event) {
-    //     console.log('File ' + event.path + ' was ' + event.type + '.');
-    // });
+    gulp.watch('source/**/*.js', ['transpile']);
 });
 
-gulp.task('default', ['watch']);
+gulp.task('release-js',
+    () => gulp.src('/source/*.js')
+        // .pipe(stripDebug())       
+        .pipe(uglify())
+        .pipe(rev())
+        .pipe(gulp.dest('/js/'))
+);
+
+gulp.task('css', function () {
+    gulp.src('css/*.css')
+        .pipe(cssmin())
+        .pipe(gulp.dest('css2'));
+});
+
+gulp.task('dangjian', () => {
+    return gulp.src('bundles.html')
+        .pipe(useref())
+        .pipe(gulpif('*.js', uglify()))
+        // .pipe(concat('common-package-js.js'))
+        // .pipe(uglify())
+        // .pipe(rev())
+        .pipe(gulp.dest('js/'))
+    // .pipe(rev.manifest('rev-js.json'))
+    // .pipe(gulp.dest('dist/public/rev/js/'));
+});
+
+gulp.task('onecss', () => {
+    return gulp.src('styles.html')
+        .pipe(useref())
+        // .pipe(concat('common-package-js.js'))
+        // .pipe(uglify())
+        // .pipe(rev())
+        .pipe(gulp.dest('css2/'))
+    // .pipe(rev.manifest('rev-js.json'))
+    // .pipe(gulp.dest('dist/public/rev/js/'));
+});
