25fc18c68c00ac41d58451221114b1937b1a42f9
xuning1734 <xuning1734@gmail.com>
2017-11-01 15:46:28 +0800

    异步请求按照请求的顺序渲染

--- asyncRender/index.js
+++ asyncRender/index.js
@@ -1,29 +1,32 @@
 var promises = {
-    asnyc1: new Promise((resolve, reject) => {
+    asnyc0: new Promise((resolve, reject) => {
         setTimeout(() => { resolve("A") }, 500)
     }),
-    asnyc2: new Promise((resolve, reject) => {
+    asnyc1: new Promise((resolve, reject) => {
         setTimeout(() => { resolve("B") }, 300)
     }),
-    asnyc3: new Promise((resolve, reject) => {
+    asnyc2: new Promise((resolve, reject) => {
         setTimeout(() => { resolve("C") }, 800)
-    }),
+    })
 }
 
 let resultArr = [];
 
 const render = (idx, data) => document.querySelector(`[data="${idx}"]`).innerHTML = data;
 
-const resultHandler = (val, data) => {
-    resultArr[val] = data;
+const resultHandler = (domIdx, data) => {
+    resultArr[domIdx] = { data, seted: false };
+    resultArr = Array.from(resultArr);
     resultArr.forEach((x, idx) => {
-        if (idx == 1) {
-            render(val, data);
-            x.seted = true;
+        if (idx == 0) {
+            if (x && x.data) {
+                render(idx, x.data);
+                x.seted = true;
+            }
             return
         }
-        else if (x[idx - 1] && x[idx - 1].seted) {
-            render(val, data);
+        else if (resultArr[idx - 1] && resultArr[idx - 1].seted && x.data) {
+            render(idx, x.data);
             x.seted = true;
             return
         }
